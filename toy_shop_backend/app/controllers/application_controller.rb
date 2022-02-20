683f9d57a0122cf7eaf49a64c1d959ee56378754
jjhiggz <jonathan.higger@gmail.com>
2022-02-19 20:08:45 -0700

    going to try authorizing request

--- toy_shop_backend/app/controllers/application_controller.rb
+++ toy_shop_backend/app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::API
   def get_token(user)
     JWT.encode(user.id, 'secret')
   end
+
+  def authorize_request
+    header = request.headers['Authorization']
+
+    begin
+      @decoded = JWT.decode(header, 'secret', true, algorithm: 'HS256')
+      @current_user_id = @decoded[0]
+      @current_user = User.find(@current_user_id)
+    rescue JWT::DecodeError
+      render json: {
+        error: {
+          message: 'Not Authorized'
+        }
+      }, status: :unauthorized
+    end
+  end
 end
